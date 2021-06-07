61f71c02df5b6662e194844f68ceb64b12d3abae
petercsauer <sauer47366>
2021-06-07 18:19:38 -0500

    added rest of assignments

---
+++ HW8/HW8.X/main.c
@@ -0,0 +1,246 @@
+#include<xc.h>           // processor SFR definitions
+#include<sys/attribs.h>  // __ISR macro
+#include<stdio.h>
+#include "i2c_master_noint.h"
+
+// DEVCFG0
+#pragma config DEBUG = OFF // disable debugging
+#pragma config JTAGEN = OFF // disable jtag
+#pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+#pragma config PWP = OFF // disable flash write protect
+#pragma config BWP = OFF // disable boot write protect
+#pragma config CP = OFF // disable code protect
+
+// DEVCFG1
+#pragma config FNOSC = FRCPLL // use internal pll
+#pragma config FSOSCEN = OFF // disable secondary oscillator
+#pragma config IESO = OFF // disable switching clocks
+#pragma config POSCMOD = OFF // RC mode
+#pragma config OSCIOFNC = OFF // disable clock output
+#pragma config FPBDIV = DIV_1 // divide sysclk freq by 1 for peripheral bus clock
+#pragma config FCKSM = CSDCMD // disable clock switch and FSCM
+#pragma config WDTPS = PS1048576 // use largest wdt
+#pragma config WINDIS = OFF // use non-window mode wdt
+#pragma config FWDTEN = OFF // wdt disabled
+#pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the sysclk clock to 48MHz from the 8MHz crystal
+#pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+#pragma config FPLLMUL = MUL_24 // multiply clock after FPLLIDIV
+#pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 48MHz
+
+// DEVCFG3
+#pragma config USERID = 0 // some 16bit userid, doesn't matter what
+#pragma config PMDL1WAY = OFF // allow multiple reconfigurations
+#pragma config IOL1WAY = OFF // allow multiple reconfigurations
+
+void readUART1(char * string, int maxLength);
+void writeUART1(const char * string);
+
+int main() {
+    
+    __builtin_disable_interrupts(); // disable interrupts while initializing things
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+    
+    // do your TRIS and LAT commands here
+    TRISBbits.TRISB4 = 1; //B4 is input
+    TRISAbits.TRISA4 = 0; //A4 is output
+    LATAbits.LATA4 = 0; //A4 is low
+    
+    U1RXRbits.U1RXR = 0b0001; // U1RX is B6
+    RPB7Rbits.RPB7R = 0b0001; // U1TX is B7
+    
+    // turn on UART3 without an interrupt
+    U1MODEbits.BRGH = 0; // set baud to NU32_DESIRED_BAUD
+    U1BRG = ((48000000 / 115200) / 16) - 1;
+
+    // 8 bit, no parity bit, and 1 stop bit (8N1 setup)
+    U1MODEbits.PDSEL = 0;
+    U1MODEbits.STSEL = 0;
+
+    // configure TX & RX pins as output & input pins
+    U1STAbits.UTXEN = 1;
+    U1STAbits.URXEN = 1;
+
+    // enable the uart
+    U1MODEbits.ON = 1;
+    
+    __builtin_enable_interrupts();
+    
+    //Initialize I2C
+    i2c_master_setup();
+    //set all A pins to output
+    
+    writePin(0x40, 0x00, 0x00);
+    
+    i2c_master_start();
+    i2c_master_send(0b01000000);  //send address + write bit 0b01000000
+    i2c_master_send(0x00);  //command register address for IODIRA
+    i2c_master_send(0x00);  //set all A pins to output
+    i2c_master_stop();
+    writePin(0x40, 0x14, 0xFF); //turn on pin GPA7
+    //set all B pins to input
+    writePin(0x40, 0x01, 0xFF);
+    i2c_master_start();
+    i2c_master_send(0x40);  //send address + write bit
+    i2c_master_send(0x01);  //command register address for IODIRB
+    i2c_master_send(0xFF);  //set all A pins to output
+    i2c_master_stop();
+    unsigned char value;
+    while(1) {
+        _CP0_SET_COUNT(0);  //reset core timer
+        LATAINV = 0b10000; //toggle pin A4
+        
+        while(_CP0_GET_COUNT() < 6000000) { ; }   //0.25s delay
+        
+        value = readPin(0b01000000, 0b01000001, 0x13, 1);
+        value &= 1;
+        char m[100];
+        sprintf(m, "%d\r\n", value);
+        writeUART1(m); //call on writeUART1 to send PIC a message
+        if (value == 0){
+            writePin(0x40, 0x14, 0xFF); //turn on pin GPA7
+        }
+        if (value == 1){
+            writePin(0x40, 0x14, 0x00); //turn off pin GPA7
+        }
+    }
+}
+
+
+// Read from UART1
+// block other functions until you get a '\r' or '\n'
+// send the pointer to your char array and the number of elements in the array
+void readUART1(char * message, int maxLength) {
+  char data = 0;
+  int complete = 0, num_bytes = 0;
+  // loop until you get a '\r' or '\n'
+  while (!complete) {
+    if (U1STAbits.URXDA) { // if data is available
+      data = U1RXREG;      // read the data
+      if ((data == '\n') || (data == '\r')) {
+        complete = 1;
+      } else {
+        message[num_bytes] = data;
+        ++num_bytes;
+        // roll over if the array is too small
+        if (num_bytes >= maxLength) {
+          num_bytes = 0;
+        }
+      }
+    }
+  }
+  // end the string
+  message[num_bytes] = '\0';
+}
+
+// Write a character array using UART3
+void writeUART1(const char * string) {
+  while (*string != '\0') {
+    while (U1STAbits.UTXBF) {
+      ; // wait until tx buffer isn't full
+    }
+    U1TXREG = *string;
+    ++string;
+  }
+}
+
+// I2C Master utilities, using polling rather than interrupts
+// The functions must be called in the correct order as per the I2C protocol
+// I2C pins need pull-up resistors, 2k-10k
+
+void i2c_master_setup(void) {
+    // using a large BRG to see it on the nScope, make it smaller after verifying that code works
+    // look up TPGD in the datasheet
+    I2C1BRG = 1000; // I2CBRG = [1/(2*Fsck) - TPGD]*Pblck - 2 (TPGD is the Pulse Gobbler Delay) 
+    //make 400kHz later on ^
+    ANSELA = 0;
+    ANSELB = 0;
+    I2C1CONbits.ON = 1; // turn on the I2C1 module
+}
+
+void i2c_master_start(void) {
+    I2C1CONbits.SEN = 1; // send the start bit
+    while (I2C1CONbits.SEN) {
+        ;
+    } // wait for the start bit to be sent
+}
+
+void i2c_master_restart(void) {
+    I2C1CONbits.RSEN = 1; // send a restart 
+    while (I2C1CONbits.RSEN) {
+        ;
+    } // wait for the restart to clear
+}
+
+void i2c_master_send(unsigned char byte) { // send a byte to slave
+    I2C1TRN = byte; // if an address, bit 0 = 0 for write, 1 for read
+    while (I2C1STATbits.TRSTAT) {
+        ;
+    } // wait for the transmission to finish
+    if (I2C1STATbits.ACKSTAT) { // if this is high, slave has not acknowledged
+        // ("I2C1 Master: failed to receive ACK\r\n");
+        while(1){   // get stuck here if the chip does not ACK back
+            LATASET = 0x10; //turn A4 on
+        } 
+    }
+}
+
+unsigned char i2c_master_recv(void) { // receive a byte from the slave
+    I2C1CONbits.RCEN = 1; // start receiving data
+    while (!I2C1STATbits.RBF) {
+        ;
+    } // wait to receive the data
+    return I2C1RCV; // read and return the data
+}
+
+void i2c_master_ack(int val) { // sends ACK = 0 (slave should send another byte)
+    // or NACK = 1 (no more bytes requested from slave)
+    I2C1CONbits.ACKDT = val; // store ACK/NACK in ACKDT
+    I2C1CONbits.ACKEN = 1; // send ACKDT
+    while (I2C1CONbits.ACKEN) {
+        ;
+    } // wait for ACK/NACK to be sent
+}
+
+void i2c_master_stop(void) { // send a STOP:
+    I2C1CONbits.PEN = 1; // comm is complete and master relinquishes bus
+    while (I2C1CONbits.PEN) {
+        ;
+    } // wait for STOP to complete
+}
+
+void writePin(unsigned char address, unsigned char reg, unsigned char value) {
+    i2c_master_start();
+    i2c_master_send(address);  //send address + write bit
+    i2c_master_send(reg);  //send command register address
+    i2c_master_send(value);  //send value
+    i2c_master_stop();
+}
+
+unsigned char readPin(unsigned char address, unsigned char address2, unsigned char reg, int ack) {
+    unsigned char value;
+    i2c_master_start();
+    i2c_master_send(address);  //send address + read bit
+    i2c_master_send(reg);  //send command register address
+    i2c_master_restart();   //send a restart
+    i2c_master_send(address2);
+    value = i2c_master_recv();  //receive byte from device
+    char m[100];
+    sprintf(m, "read to here\r\n");
+    writeUART1(m); //call on writeUART1 to send PIC a message
+    i2c_master_ack(ack);  //send acknowledge bit
+    i2c_master_stop();
+    return value;
+}
